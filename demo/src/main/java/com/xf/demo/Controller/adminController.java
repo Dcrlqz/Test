2813c38b7539288aa89d33fc96036497217ae1c1
Administrator <2335670981@qq.com>
2021-03-25 09:44:53 +0800

    新增查看管理员信息接口

---
+++ demo/src/main/java/com/xf/demo/Controller/adminController.java
@@ -0,0 +1,15 @@
+package com.xf.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class adminController {
+
+    @GetMapping("/adminInfo")
+    public String admin(){
+        return "这是用户信息";
+    }
+}
